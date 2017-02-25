d155c1d1919a2a4f5bd6e0d048d04140f57e9058
User1 <User1@DESKTOP-LIK6KBF>
2017-02-25 12:31:11 -0700

    Add project files.

---
+++ cstest/cstest.Droid/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace cstest.Droid
+{
+	[Activity (Label = "cstest.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            MyClass mc = new MyClass();
+            FindViewById<TextView>(Resource.Id.st).Text = mc.Thing1;
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+            button.Click += Button_Click;
+		}
+        private async void Button_Click(object sender, EventArgs e)
+        {
+            MyClass mc = new MyClass();
+            count = (count +1)%2;
+            if(count == 1)
+                FindViewById<TextView>(Resource.Id.st).Text = mc.Thing1;
+            else
+                FindViewById<TextView>(Resource.Id.st).Text = mc.thing2;
+        }
+	}
+}
+
+
---
+++ cstest/cstest/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cstest
+{
+    public class MyClass
+    {
+        public string Thing1 {get;set;}
+        public string thing2 { get; set; }
+        public MyClass()
+        {
+            Thing1 = "Blank thing 1";
+            thing2 = "blank thing 2";
+        }
+
+    }
+}
+
